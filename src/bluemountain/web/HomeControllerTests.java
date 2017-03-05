72c34882d6431ae436a8975d18476588e16531e0
CMK <cirno.mainasuk@gmail.com>
2017-03-05 11:45:18 +0800

    feat: add home page test

---
+++ src/bluemountain/web/HomeControllerTests.java
@@ -0,0 +1,24 @@
+package bluemountain.web;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+/**
+ * Created by MainasuK on 2017-3-5.
+ */
+
+public class HomeControllerTests {
+
+    @Test
+    public void testHomePage() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc mvc = standaloneSetup(controller).build();
+
+        mvc.perform(get("/")).andExpect(view().name("other"));
+    }
+
+}
